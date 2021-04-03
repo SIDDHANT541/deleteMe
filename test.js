b6124b684f778f605162442eac2c9095e107ed35
SIDDHANT541 <onlysiddhant1998@gmail.com>
2021-04-03 09:30:20 +0530

    fourth revert

--- test.js
+++ test.js
@@ -3,3 +3,12 @@ console.log("Hello World 2");
 console.log("Hello World 3");
 console.log("Hello World 4");
 console.log("Hello World 5");
+<<<<<<< HEAD
+=======
+console.log("Hello World 6");
+console.log("Hello World 7");
+console.log("Hello World 8");
+console.log("Hello World 9");
+console.log("Hello World 10");
+
+>>>>>>> parent of 6f18c86 (third modified)
